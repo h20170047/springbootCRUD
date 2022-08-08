c9e4780d2bae06d16c2056be7cb4d41832ef1fcd
h20170047 <46812751+h20170047@users.noreply.github.com>
2022-08-08 13:27:45 +0530

    adding employees entity,CRUD endpoints, postgres integration

---
+++ src/main/java/com/svj/springbootcrud/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.svj.springbootcrud.repository;
+
+import com.svj.springbootcrud.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long>{
+
+}
